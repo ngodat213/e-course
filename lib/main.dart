c1a987f7e1e5fa8a896a39d9e58c3158d304d046
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2023-08-25 13:16:33 +0700

    init app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz Flutter',
+      debugShowCheckedModeBanner: false, // set false debug banner
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
